2d854a07b78b3ac05284ffea09a12259e3900798
Xinyue Yang <76737318+72414@users.noreply.github.com>
2023-05-11 12:35:13 +0800

    Update Interpolation.m

--- Interpolation.m
+++ Interpolation.m
@@ -1,7 +1,7 @@
  %Linear interpolation
 long=size(X,1)
 a=[1:1:long];
-aa=linspace(1,long,360000) %在356828个数据点中取(360000)[根据保留时间决定]
+aa=linspace(1,long,360000) 
 AAA=[]
 MS_long=365
 for i=1:1:MS_long
